39d89e738e55d2b63ef963df27d5f23a62943062
inku0 <ingvar@saarend.com>
2026-04-25 01:53:39 +0300

    blindly using the first result and praying

--- listener.go
+++ listener.go
@@ -68,7 +68,7 @@ func listener(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatalf("failed to look up movie: %s: %s", payload.Name, err)
 		}
-		log.Printf("%+v", lookup)
+		log.Printf("%+v", lookup[0])
 		_, err = s.Radarr.EditMovies(&radarr.BulkEdit{
 			MovieIDs:  []int64{lookup[0].ID},
 			Monitored: starr.False(),
@@ -82,7 +82,7 @@ func listener(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatalf("failed to look up series: %s", err)
 		}
-		log.Printf("%+v", lookup)
+		log.Printf("%+v", lookup[0])
 		_, err = s.Sonarr.UpdateSeries(&sonarr.AddSeriesInput{
 			Monitored: false,
 			ID:        lookup[0].ID,
