df1bdc20413a282bc30d1716770a6995bdc0a284
inku0 <ingvar@saarend.com>
2026-04-29 00:30:06 +0300

    big refactor; multithreading

--- handler.go
+++ handler.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"slices"
+	"sync"
 
 	"github.com/razsteinmetz/go-ptn"
 	"github.com/superturkey650/go-qbittorrent/qbt"
@@ -60,7 +61,7 @@ func (h *WebhookHandler) Handle(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (h *WebhookHandler) findMovie(name string) (*radarr.Movie, error) {
+func (h *WebhookHandler) findMovie(name string) ([]*radarr.Movie, error) {
 	lookup, err := h.Radarr.Lookup(name)
 	if err != nil {
 		return nil, err
@@ -69,19 +70,37 @@ func (h *WebhookHandler) findMovie(name string) (*radarr.Movie, error) {
 		return nil, fmt.Errorf("found 0 results for \"%s\" in Radarr", name)
 	}
 
-	return lookup[0], nil
+	return lookup, nil
 }
 
-func (h *WebhookHandler) findSeries(name string) (*sonarr.Series, error) {
-	lookup, err := h.Sonarr.Lookup(name)
+// TODO this finds serieses which aren't monitored
+func (h *WebhookHandler) findSeries(name string) ([]*sonarr.Series, error) {
+	all, err := h.Sonarr.GetAllSeries()
 	if err != nil {
 		return nil, err
 	}
-	if len(lookup) == 0 {
+
+	if len(all) == 0 {
 		return nil, fmt.Errorf("found 0 results for \"%s\" in Sonarr", name)
 	}
 
-	return lookup[0], nil
+	names := make([]string, 0, len(all))
+	for _, series := range all {
+		names = append(names, series.Title)
+	}
+
+	matches := Find(name, names)
+	if len(matches) == 0 {
+		return nil, fmt.Errorf("matched 0 results for \"%s\" in Sonarr", name)
+	}
+
+	serieses := make([]*sonarr.Series, 0, len(matches))
+	for _, match := range matches {
+		index := slices.Index(names, match.Str)
+		serieses = append(serieses, all[index])
+	}
+
+	return serieses, nil
 }
 
 func (h *WebhookHandler) findTag(name string, service string) (int, error) {
@@ -111,6 +130,16 @@ func (h *WebhookHandler) findTag(name string, service string) (int, error) {
 	return -1, fmt.Errorf("found no results for tag \"%s\"", name)
 }
 
+func parseAll(w int, jobs <-chan qbt.TorrentInfo, results chan<- map[string][]string) {
+	parses := make(map[string][]string)
+	for j := range jobs {
+		parsed, _ := ptn.Parse(j.Name)
+
+		parses[parsed.Title] = append(parses[parsed.Title], j.Hash)
+	}
+	results <- parses
+}
+
 func (h *WebhookHandler) processp(p Deletion) error {
 	var service string
 	if p.ItemType == "Movie" {
@@ -121,15 +150,20 @@ func (h *WebhookHandler) processp(p Deletion) error {
 		return fmt.Errorf("unsupported ItemType: %s", p.ItemType)
 	}
 
-	var name string
+	var titles []string
 	if service == "Radarr" {
 		log.Printf("finding %s", p.Name+" "+p.Year)
 		n, err := h.findMovie(p.Name + " " + p.Year)
 		if err != nil {
 			return err
 		}
-		name = n.Title
-		log.Printf("found %s as %s", p.Name+" "+p.Year, name)
+		log.Printf("found %s as", p.Name+" "+p.Year)
+		var ids []int64
+		for _, movie := range n {
+			ids = append(ids, movie.ID)
+			titles = append(titles, movie.Title)
+			fmt.Printf("%s\n", movie.Title)
+		}
 
 		tag, err := h.findTag("marked-for-death", service)
 		if err != nil {
@@ -139,7 +173,7 @@ func (h *WebhookHandler) processp(p Deletion) error {
 		if os.Getenv("DRY_RUN") == "false" {
 			_, err = h.Radarr.EditMovies(&radarr.BulkEdit{
 				Monitored: starr.False(),
-				MovieIDs:  []int64{n.ID},
+				MovieIDs:  ids,
 				Tags:      []int{tag},
 			})
 			if err != nil {
@@ -148,46 +182,48 @@ func (h *WebhookHandler) processp(p Deletion) error {
 		}
 
 	} else if service == "Sonarr" {
-		log.Printf("finding %s", p.Name+" "+p.Year)
-		n, err := h.findSeries(p.Name + " " + p.Year)
+		log.Printf("finding %s", p.Name)
+		n, err := h.findSeries(p.Name)
 		if err != nil {
 			return err
 		}
-		name = n.Title
-		log.Printf("found %s as %s", p.Name+" "+p.Year, name)
+		//log.Printf("found %s as %+v", p.Name)
 
 		tag, err := h.findTag("marked-for-death", service)
 		if err != nil {
 			return err
 		}
 
-		tags := append(n.Tags, tag)
+		for _, series := range n {
+			tags := append(series.Tags, tag)
+			titles = append(titles, series.Title)
 
-		input := sonarr.AddSeriesInput{
-			Monitored:         false,
-			SeasonFolder:      n.SeasonFolder,
-			UseSceneNumbering: n.UseSceneNumbering,
-			ID:                n.ID,
-			LanguageProfileID: n.LanguageProfileID,
-			QualityProfileID:  n.QualityProfileID,
-			TvdbID:            n.TvdbID,
-			ImdbID:            n.ImdbID,
-			TvMazeID:          n.TvMazeID,
-			TvRageID:          n.TvRageID,
-			Path:              n.Path,
-			SeriesType:        n.SeriesType,
-			Title:             n.Title,
-			TitleSlug:         n.TitleSlug,
-			RootFolderPath:    n.RootFolderPath,
-			Tags:              tags,
-			Seasons:           n.Seasons,
-			Images:            n.Images,
-		}
-		fmt.Printf("%+v", input)
-		if os.Getenv("DRY_RUN") == "false" {
-			_, err = h.Sonarr.UpdateSeries(&input, false)
-			if err != nil {
-				return err
+			input := sonarr.AddSeriesInput{
+				Monitored:         false,
+				SeasonFolder:      series.SeasonFolder,
+				UseSceneNumbering: series.UseSceneNumbering,
+				ID:                series.ID,
+				LanguageProfileID: series.LanguageProfileID,
+				QualityProfileID:  series.QualityProfileID,
+				TvdbID:            series.TvdbID,
+				ImdbID:            series.ImdbID,
+				TvMazeID:          series.TvMazeID,
+				TvRageID:          series.TvRageID,
+				Path:              series.Path,
+				SeriesType:        series.SeriesType,
+				Title:             series.Title,
+				TitleSlug:         series.TitleSlug,
+				RootFolderPath:    series.RootFolderPath,
+				Tags:              tags,
+				Seasons:           series.Seasons,
+				Images:            series.Images,
+			}
+			//fmt.Printf("%+v", input)
+			if os.Getenv("DRY_RUN") == "false" {
+				_, err = h.Sonarr.UpdateSeries(&input, false)
+				if err != nil {
+					return err
+				}
 			}
 		}
 	}
@@ -206,27 +242,81 @@ func (h *WebhookHandler) processp(p Deletion) error {
 		return err
 	}
 
+	// source: https://gobyexample.com/worker-pools
+	numJobs := len(torrents)
+
 	nameHashes := make(map[string][]string, len(torrents))
-	for _, torrent := range torrents {
-		parsed, err := ptn.Parse(torrent.Name)
-		if err != nil {
-			return err
-		}
-		nameHashes[parsed.Title] = append(nameHashes[parsed.Title], torrent.Hash)
+
+	type result struct {
+		title string
+		hash  string
+		err   error
 	}
+
+	jobs := make(chan qbt.TorrentInfo, numJobs)
+	results := make(chan result, numJobs)
+
+	workers := 8
+	var wg sync.WaitGroup
+	wg.Add(workers)
+
+	for i := 0; i < workers; i++ {
+		go func() {
+			defer wg.Done()
+			for torrent := range jobs {
+				parsed, err := ptn.Parse(torrent.Name)
+				if err != nil {
+					results <- result{err: err}
+					continue
+				}
+				results <- result{title: parsed.Title, hash: torrent.Hash}
+			}
+		}()
+	}
+
+	// wrap in go func() so that they can run concurrently
+	// feed jobs
+	go func() {
+		for _, torrent := range torrents {
+			jobs <- torrent
+		}
+		close(jobs)
+	}()
+
+	// close results after workers finish
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	// this also run concurrently so a mutex is needed
+	var mu sync.Mutex
+	for res := range results {
+		if res.err != nil {
+			return res.err
+		}
+		mu.Lock()
+		nameHashes[res.title] = append(nameHashes[res.title], res.hash)
+		mu.Unlock()
+	}
+
 	log.Printf("parsed torrent names")
 
 	names := slices.Sorted(maps.Keys(nameHashes))
 
-	matches := Find(name, names)
+	var matches Matches
+	for _, title := range titles {
+		matches = append(matches, Find(title, names)...) // is this legal? (...)
+	}
+	//matches := Find(name, names)
 	if len(matches) == 0 {
-		return fmt.Errorf("found no matches for %s in qBittorrent", name)
+		return fmt.Errorf("found no matches for %+v in qBittorrent", titles)
 	}
 
 	hashes := make([]string, 0)
 
 	for _, match := range matches {
-		log.Printf("matched %s to %s with score %d", name, match.Str, match.Score)
+		log.Printf("matched %+v to %s with score %d", titles, match.Str, match.Score)
 		hashes = append(hashes, nameHashes[match.Str]...)
 	}
 
